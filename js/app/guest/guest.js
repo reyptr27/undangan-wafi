846c30b43c14fc5e7079bd10ded81d17e56200c9
unknown <Reynaldo Putra@Laptop-28.hfmi.local>
2025-12-30 02:18:35 +0700

    release v1.0

--- js/app/guest/guest.js
+++ js/app/guest/guest.js
@@ -255,10 +255,10 @@ export const guest = (() => {
         const url = new URL('https://calendar.google.com/calendar/render');
         const data = new URLSearchParams({
             action: 'TEMPLATE',
-            text: 'The Wedding of Wahyu and Riski',
-            dates: `${formatDate('2023-03-15 10:00')}/${formatDate('2023-03-15 11:00')}`,
+            text: 'The Wedding of Wafi and Naura',
+            dates: `${formatDate('2026-01-11 09:00')}/${formatDate('2026-01-11 12:00')}`,
             details: 'Tanpa mengurangi rasa hormat, kami mengundang Anda untuk berkenan menghadiri acara pernikahan kami. Terima kasih atas perhatian dan doa restu Anda, yang menjadi kebahagiaan serta kehormatan besar bagi kami.',
-            location: 'RT 10 RW 02, Desa Pajerukan, Kec. Kalibagor, Kab. Banyumas, Jawa Tengah 53191.',
+            location: 'Jl. Raya Permata No.7, Kembangan, Kec. Kebomas, Kabupaten Gresik, Jawa Timur 61124',
             ctz: config.get('tz'),
         });
 
